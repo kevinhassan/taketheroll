86d5384a97db353cdc2fe17ac063578b6a0d44fb
kevinhassanIG <kevin.hassan@etu.umontpellier.fr>
2016-05-05 20:57:10 +0200

    [REMOVE]Nodejs Tokken example

--- routes/index.js
+++ routes/index.js
@@ -1,31 +1,21 @@
-/**
-* ./routes/index.js
-* Contains all the rootes needed for my app
-* 3 types of routes
-*/
+/*./routes/index.js*/
 var express = require('express');
 var router = express.Router();
-var auth = require('./auth');
-var products = require('./products');
-var user = require('./users');
-/*
-* Routes that can be accessed by any one
-*/
-router.post('/login', auth.login);
-/*
-* Routes that can be accessed only by authenticated users
-*/
-router.get('/api/v1/products', products.getAll);
-router.get('/api/v1/product/:id', products.getOne);
-router.post('/api/v1/product/', products.create);
-router.put('/api/v1/product/:id', products.update);
-router.delete('/api/v1/product/:id', products.delete);
-/*
-* Routes that can be accessed only by authenticated & authorized users
-*/
-router.get('/api/v1/admin/users', user.getAll);
-router.get('/api/v1/admin/user/:id', user.getOne);
-router.post('/api/v1/admin/user/', user.create);
-router.put('/api/v1/admin/user/:id', user.update);
-router.delete('/api/v1/admin/user/:id', user.delete);
+
+router.get("/",function(req,res){
+  res.status(200);
+  res.json({"message" : "Bienvenu à l'accueil"});
+});
+router.get("/login/student",function(req,res){
+    res.status(200);
+    res.json({"role":student,"message" : "Veuillez vous connecter"});
+});
+router.get("/login/secretariat",function(req,res){
+    res.status(200);
+    res.json({"role":secretariat,"message" : "Veuillez vous connecter"});
+});
+router.get("/login/teacher",function(req,res){
+    res.status(200);
+    res.json({"role":teacher,"message" : "Veuillez vous connecter"});
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -6,37 +6,28 @@ var pg = require('pg');//postgres module
 var express = require('express');
 var path = require('path');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
-app.all('/*', function(req, res, next) {
-  // CORS headers
-  res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // Set custom headers for CORS
-  res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-  if (req.method == 'OPTIONS') {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
-// This middleware allow to authenticate and authorize request
-app.all('/api/v1/*', [require('./middlewares/validateRequest')]);
-// The list of routes for the application
-app.use('/', require('./routes'));
-// If no route is matched by now, it must be a 404
+// La liste des routes de l'application
+app.use('/', routes);
+// Si la page n'existe pas envoyer une erreur 404
 app.use(function(req, res, next) {
-var err = new Error('Not Found');
-err.status = 404;
-next(err);
+  res.status(404);
+  next();
 });
-// Start the server
+
+// Démarrer le server
 app.set('port', process.env.PORT || 8080);
 var server = app.listen(app.get('port'), function() {
-console.log('Express server listening on port ' + server.address().port);
+  console.log('Express server listening on port ' + server.address().port);
 });
