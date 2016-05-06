59ff246ff35bb7e1c2a8d8568362735f1f608b78
kevinhassanIG <kevin.hassan@etu.umontpellier.fr>
2016-05-06 14:09:57 +0200

    [ADD] All routes in routes/index.js
    4 types of routes : 
    - accessible for all (/ or /login)
    - accessible by teacher
    - accessible by student
    - accessible by secretariat

--- routes/index.js
+++ routes/index.js
@@ -30,21 +30,28 @@ router.get('/student/late/:id', late.getOne);
 router.put('/student/late/:id', late.update);//Justifier retard
 
 //Seul les professeurs peuvent y accéder
-
-
-/*
- * Routes that can be accessed only by authenticated & authorized users
- */
-
-
+router.get('/teacher/students',student.getAll);
+router.post('teacher/students/roll',teacher.takeTheRoll);//Faire l'appel sur la liste des étudiants
+router.put('teacher/students/roll/:id', student.switchToLate);//Passer d'une absence a un retard
 //Seul le secrétariat peut y accéder
 /**
 * Gérer les étudiants
 */
-router.get('/admin/student', student.getAll);
+router.get('/admin/students', student.getAll);
 router.get('/admin/student/:id', student.getOne);
 router.post('/admin/student/', student.create);
 router.put('/admin/student/:id', student.update);
 router.delete('/admin/student/:id', student.delete);
+//Consuler les alertes récentes
+router.get('/admin/alert',admin.alert);
+//Gérer les absences
+router.get('/admin/students/absences',absence.getAll);
+router.get('/admin/students/absences/:id',absence.justify);
+router.get('/admin/students/absences',absence.delete);
+router.get('/admin/students/absences',absence.add);//Ajouter une absence justifier par l'éléve
+//Gérer les retards
+router.get('/admin/students/lates',late.getAll);
+router.get('/admin/students/lates/:id',late.justify);
+router.get('/admin/students/lates',absence.delete);
 
 module.exports = router;
