8dddad827b0935023f336db89836675891ce399c
kevinhassanIG <kevin.hassan@etu.umontpellier.fr>
2016-05-08 13:24:57 +0200

    [ADD]Create query to model

--- models/model.js
+++ models/model.js
@@ -10,6 +10,21 @@ exports.deleteWhere = function(table,pk,id){
   var sql = "DELETE FROM "+table+" WHERE "+ pk+"="+id;
   return sql;
 }
+exports.create = function(table,data){
+  var sql = "INSERT INTO "+table;
+  var keys = " (";
+  var values ="";
+  for(var key in data) {
+    keys += key+",";
+    values +="'"+data[key]+"',";
+  }
+  keys = keys.substring(0, keys.length - 1);//On enlève le ',' en trop
+  values = values.substring(0, values.length - 1);//On enlève le ',' en trop
+  keys +=") VALUES (";
+  values +=")";
+  sql += keys + values;
+  return sql;
+}
 exports.update= function(table,updateData,pk,id){
   var sql = "UPDATE "+table+" SET ";
   for(var key in updateData) {
@@ -17,6 +32,5 @@ exports.update= function(table,updateData,pk,id){
   }
   sql =  sql.substring(0, sql.length - 1);//On enlève le ',' en trop
   sql += "WHERE "+pk+"="+id;
-  return sql
-  //return sql;
+  return sql;
 }
--- models/student.js
+++ models/student.js
@@ -16,29 +16,17 @@ var student = {
     res.send(result);
   },
   create: function(req, res){
-    var result = db.query("INSERT INTO student (id,name,nickname) VALUES("+req.body.id+",'"+req.body.name+"','"+req.body.nickname+"')");//Requête de test
+    var newStudent = {'id':4,'name':"kash",'nickname':"kake"};//Requête de test
+    var sql = model.create(table,newStudent);
+    var result = db.query(sql);
     res.send(result);
-    /*var result = db.query('INSERT INTO student'
-              + ' (nom, prenom, sexe, numTel, email, adresse, dateNaissance, nomUtilisateur, motDepasse)'
-              + ' VALUES '
-              + ' ($1, $2, $3, $4, $5, $6, $7, $8)'
-           , [newStudent.nom, newStudent.prenom, newStudent.sexe, newStudent.numTel, newStudent.email,
-             newStudent.adresse, newStudent.dateNaissance, newStudent.email, hash(newStudent.dateNaissance)]);//On initialise le mdp à la date de naissance
-    */
   },
   update: function(req, res){
     var id = req.params.id;
     var updateData = {'id':4,'name':"kash",'nickname':"kake"};//A recupèrer dans le formulaire
     var sql = model.update(table,updateData,pk,id);
-    console.log(sql);
     var result = db.query(sql);//Requête de test
     res.send(result);
-    /*db.query('UPDATE student'
-              + 'SET nom=updateData.nom, prenom=updateData.prenom , sexe=updateData.sexe,'
-              +'numTel=updateData.numTel, email=updateData.email , adresse=updateData.adresse,'
-              +'dateNaissance= updateData.dateNaissance, nomUtilisateur=updateData.email'
-              + 'WHERE id='
-              + id);*/
   },
   delete: function(req, res){
     var id = req.params.id;
