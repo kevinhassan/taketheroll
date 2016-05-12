cd05eb01683d9da0a4b4b186c1babb9fe528198e
kevinhassanIG <kevin.hassan@etu.umontpellier.fr>
2016-05-12 21:07:53 +0200

    [FIX]Triggers

--- server/config/triggers.sql
+++ server/config/triggers.sql
@@ -1,22 +1,28 @@
 -- Regarder dans la table absence si à la creation du retard l'absence est présente
 -- Dans ce cas supprimer l'absence et ajouter le retard
 
-CREATE FUNCTION before_insert_late() RETURNS trigger AS $before_insert_late$
+CREATE OR REPLACE FUNCTION before_insert_late() RETURNS trigger AS $before_insert_late$
   BEGIN
-    DELETE FROM absence WHERE "id_Course"=NEW.id_Course AND "id_Student"=NEW.id_Student;
+    DELETE FROM absence
+    WHERE absence.id_Course=NEW.late.id_Course
+    AND absence.id_Student=NEW.late.id_Student;
     RETURN NEW;
   END;
   $before_insert_late$ LANGUAGE plpgsql;
   CREATE TRIGGER before_insert_late BEFORE INSERT ON late
-    FOR EACH ROW EXECUTE PROCEDURE before_insert_late();
+    FOR EACH ROW
+    EXECUTE PROCEDURE before_insert_late();
 
--- Ajouter un utilisateur après avoir créé un étudiant ou un professeur ou un administrateur
+-- Ajouter une date lors de la création d'un utilisateur
 
-  CREATE FUNCTION after_insert_user() RETURNS trigger AS $after_insert_user$
+  CREATE OR REPLACE FUNCTION after_insert_user() RETURNS trigger AS $after_insert_user$
     BEGIN
-      --Ajouter date 
+      UPDATE users
+      SET users.creation_date = now()
+      WHERE users.id_User= users.NEW.id_User;
       RETURN NULL;--Ignoré car trigger AFTER
     END;
     $after_insert_user$ LANGUAGE plpgsql;
-    CREATE TRIGGER before_insert_user AFTER INSERT ON users
-      FOR EACH ROW EXECUTE PROCEDURE after_insert_user();
+    CREATE TRIGGER after_insert_user AFTER INSERT ON users
+      FOR EACH ROW
+      EXECUTE PROCEDURE after_insert_user();
