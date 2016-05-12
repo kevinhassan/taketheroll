c22c8c4d897ddf059e8a443f5fd22fb2438ab770
kevinhassanIG <kevin.hassan@etu.umontpellier.fr>
2016-05-12 19:27:51 +0200

    [ADD]Trigger 2

--- server/config/triggers.sql
+++ server/config/triggers.sql
@@ -7,6 +7,16 @@ CREATE FUNCTION before_insert_late() RETURNS trigger AS $before_insert_late$
     RETURN NEW;
   END;
   $before_insert_late$ LANGUAGE plpgsql;
-
   CREATE TRIGGER before_insert_late BEFORE INSERT ON late
     FOR EACH ROW EXECUTE PROCEDURE before_insert_late();
+
+-- Ajouter un utilisateur après avoir créé un étudiant ou un professeur ou un administrateur
+
+  CREATE FUNCTION after_insert_user() RETURNS trigger AS $after_insert_user$
+    BEGIN
+      --Ajouter date 
+      RETURN NULL;--Ignoré car trigger AFTER
+    END;
+    $after_insert_user$ LANGUAGE plpgsql;
+    CREATE TRIGGER before_insert_user AFTER INSERT ON users
+      FOR EACH ROW EXECUTE PROCEDURE after_insert_user();
